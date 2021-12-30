dbbc24b4bb7d54bb6c21ac11526ada9285550db6
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-12-30 17:53:04 -0300

    first commit

---
+++ javascript/introducao/to-do-list/scripts.js
@@ -0,0 +1,12 @@
+function insertDo() {
+    //pegar dados do input
+    let input = document.querySelector('#descriptionInput')
+    let inputText = input.value;
+
+    var wrapper = document.querySelector(".wrapper");
+    
+    //limpar input
+    input.value = ''
+    
+    console.log(inputText)
+}
