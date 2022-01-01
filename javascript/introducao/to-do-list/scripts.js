490307361ba9a198b879daa7f88c6b0cfd1c975e
Bruno Sobral <89643847+BrunoSobralDEV@users.noreply.github.com>
2022-01-01 11:58:38 -0300

    botão com img e função inserir texto

--- javascript/introducao/to-do-list/scripts.js
+++ javascript/introducao/to-do-list/scripts.js
@@ -1,12 +1,36 @@
 function insertDo() {
     //pegar dados do input
-    let input = document.querySelector('#descriptionInput')
-    let inputText = input.value;
+    const inputField = document.querySelector('#inputDescription')
+    const inputText  = inputField.value;
 
-    var wrapper = document.querySelector(".wrapper");
+    if(inputText != ''){
+        createDivCheckbox(inputText)
+        
+        //limpar input
+        inputField.value = ''
+    }
+}
+
+function createDivCheckbox(inputText) {
+    //Referência - elemento pai
+    let taskList = document.querySelector("#list");
     
-    //limpar input
-    input.value = ''
+    //Criar na memória
+    let newDiv       = document.createElement('div');
+    let newInput     = document.createElement('input');
+    let newLabel     = document.createElement('label');
+    let newLabelText = document.createTextNode(inputText);
     
-    console.log(inputText)
+    //Editar atributos
+    newInput.setAttribute('type', 'checkbox');
+    newInput.setAttribute('name', inputText);
+    newInput.setAttribute('id', inputText);
+
+    newLabel.setAttribute('for', inputText);
+    newLabel.appendChild(newLabelText);
+
+    //Inserir no html
+    taskList.appendChild(newDiv);
+    newDiv.appendChild(newInput);
+    newDiv.appendChild(newLabel);
 }
